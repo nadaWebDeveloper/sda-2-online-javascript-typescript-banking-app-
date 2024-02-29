e970c5018b433a26ef7ba92ba1912d5b0144be0b
nada <nadayahya2020@hotmail.com>
2024-02-29 03:10:07 -0800

    Done add class customer

--- bank.js
+++ bank.js
@@ -1,19 +1,58 @@
 // 1- CLASS TRANSACTION
 class Transaction {
-    amount;
-    date;
-    constructor(amount) {
-     if (amount < 0) {
-        throw("Transaction can not have negative amount");
-      }
-     try {
-        this.amount = amount;
-        this.date =  new Date();
-     } catch (error) {
-        console.error(error);
-     }
-    }
-  
+  constructor(amount) {
+    if (amount < 0) {
+        console.error(`Transaction can not have negative amount ${amount}`);
+    }else{    
+    try {
+      this.amount = amount;
+      this.date = new Date();
+    } catch (error) {
+      console.error(error);
+    }}
   }
-  
-  const t1 = new Transaction(90);
+}
+
+// 1- CLASS CUSTOMER
+class Customer {
+  constructor(name, id) {
+    this.name = name;
+    this.id = id;
+    this.Transaction = [];
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  getId() {
+    return this.id;
+  }
+
+  getTransaction() {
+    return this.Transaction;
+  }
+
+  getBalance(){
+    let balance = this.Transaction.reduce((total,transaction)=> total + transaction.amount,0);
+    return `Total of Balance is: ${balance}`
+  }
+
+  addTransaction(amount){
+      const transaction = new Transaction(amount);
+      const result = amount < 0 ? false :this.Transaction.push(transaction);
+      const transactionString = JSON.stringify(this.Transaction);
+      const isDone = result ? `\nThe transaction is successful for this amount[ ${amount} ]\n${transactionString}\n`:`\nThe transaction is Failed for this amount[ ${amount} ]\n`;
+      return isDone;
+  }
+}
+
+
+const c2 = new Customer('nada', '11283');
+c2.addTransaction(8970);
+const gg=c2.addTransaction(960);
+const hhh=c2.addTransaction(-675);
+const ll =c2.getBalance();
+console.log(ll);
+console.log(gg);
+console.log(hhh);
--- src/bank.ts
+++ src/bank.ts
@@ -1,22 +1,22 @@
 // 1- CLASS TRANSACTION
-class Transaction {
-  amount: number;
-  date: Date;
-  constructor(amount: number, date: Date = new Date()) {
-    if (amount < 0) {
-      throw new Error("Transaction can not have negative amount");
-    }
-    this.amount = amount;
-    this.date = date;
-  }
-  getAmount(): number {
-    return this.amount;
-  }
+// class Transaction {
+//   amount: number;
+//   date: Date;
+//   constructor(amount: number, date: Date = new Date()) {
+//     if (amount < 0) {
+//       throw new Error("Transaction can not have negative amount");
+//     }
+//     this.amount = amount;
+//     this.date = date;
+//   }
+//   getAmount(): number {
+//     return this.amount;
+//   }
 
-  getDate(): Date {
-    return this.date;
-  }
-}
+//   getDate(): Date {
+//     return this.date;
+//   }
+// }
 
-const t1 = new Transaction(-90);
-t1.getAmount();
+// const t1 = new Transaction(-90);
+// t1.getAmount();
