ad1f89a7e66c9420ebd7dee90126f6b1a5edb23e
nada <nadayahya2020@hotmail.com>
2024-02-29 11:22:59 -0800

    Done class bank

--- bank.js
+++ bank.js
@@ -57,59 +57,113 @@ class Customer {
 
 // 3- CLASS BRANCH
 class Branch {
-  constructor(nameB) {
-    this.nameB = nameB;
+  constructor(nameBranch) {
+    this.nameBranch = nameBranch;
     this.Customers = [];
   }
 
-  getBranchName(){
-    return this.nameB;
+  getBranchName() {
+    return this.nameBranch;
   }
 
-  getCustomers(){
+  getCustomers() {
     return this.Customers;
   }
 
-  addCustomer(customer){
+  addCustomer(customer) {
     const existingCustomer = this.Customers.includes(customer);
     console.log(existingCustomer);
     if (!existingCustomer) {
-        this.Customers.push(customer);
+      this.Customers.push(customer);
       return this.Customers;
     } else {
       return `customer already exists`;
     }
 
+    // if(!this.Customers.includes(customer)){
+    //     const result = this.Customers.push(customer);
+    //     return result> 0 ? true: false;
+    // }
+  }
+
+  addCustomerTransaction(customerId, amount) {
+    const customer = this.Customers.find(
+      (customer) => customer.id === customerId
+    );
+    customer ? customer.addTransaction(amount) : false;
+  }
+}
+
+// 4- CLASS BANK
+class Bank {
+  constructor(nameBank) {
+    this.nameBank = nameBank;
+    this.Branches = [];
+  }
+
+  addBranch(branch){
+  if(!this.Branches.includes(branch)){
+        const result = this.Branches.push(branch);
+        return result> 0 ? true: false;
+    }
+  }
+
+  addCustomer(branch, customer){
+    if(this.Branches.includes(branch)){
+        const result = branch.addCustomer(customer);
+        return result > 0 ? true: false;
+    }
+  }
+
+  addCustomerTransaction(branch, customerId, amount){
+   const targetBranch = this.findBranchByName(branch.nameBranch);
+   if(targetBranch){
+    targetBranch.addCustomerTransaction(customerId,amount);
+   }
 
   }
 
-  addCustomerTransaction(customerId, amount){
-     const customer = this.Customers.find((customer) => customer.id === customerId);
-     customer? customer.addTransaction(amount): false;
+  findBranchByName(branchName){
+    return this.Branches.find((branch) => branch.nameBranch === branchName);
+  }
+
+  checkBranch(branch){
+    return this.Branches.includes(branch)
+  }
+
+  listCustomers(branch, includeTransactions){
 
   }
 }
 
 
-// const arizonaBank = new Bank("Arizona")
-const customer1 = new Customer("980", '980')
-const customer2 = new Customer("Anna", 2)
 
+const arizonaBank = new Bank("Arizona")
 const westBranch = new Branch("West Branch")
 const sunBranch = new Branch("Sun Branch")
-console.log(westBranch.addCustomer('980'));
+const customer1 = new Customer("John", 1)
+const customer2 = new Customer("Anna", 2)
+const customer3 = new Customer("John", 3)
 
-// const customer2 = new Customer("Anna", 2)
-// const customer3 = new Customer("John", 3)
+arizonaBank.addBranch(westBranch)
+arizonaBank.addBranch(sunBranch)
+arizonaBank.addBranch(westBranch) 
 
-// arizonaBank.addBranch(westBranch)
-// arizonaBank.addBranch(sunBranch)
-// arizonaBank.addBranch(westBranch) 
+arizonaBank.findBranchByName("bank")
+arizonaBank.findBranchByName("sun")
 
-// arizonaBank.findBranchByName("bank")
-// arizonaBank.findBranchByName("sun")
+arizonaBank.addCustomer(westBranch, customer1)
+arizonaBank.addCustomer(westBranch, customer3)
+arizonaBank.addCustomer(sunBranch, customer1)
+arizonaBank.addCustomer(sunBranch, customer2)
+console.log(arizonaBank);
 
-// arizonaBank.addCustomer(westBranch, customer1)
-// arizonaBank.addCustomer(westBranch, customer3)
-// arizonaBank.addCustomer(sunBranch, customer1)
-// arizonaBank.addCustomer(sunBranch, customer2)
+arizonaBank.addCustomerTransaction(westBranch, customer1.getId, 3000)
+arizonaBank.addCustomerTransaction(westBranch, customer1.getId, 2000)
+arizonaBank.addCustomerTransaction(westBranch, customer2.getId, 3000)
+
+customer1.addTransaction(-1000)
+console.log(customer1.getBalance())
+console.log(arizonaBank.listCustomers(westBranch, true))
+console.log(arizonaBank.listCustomers(sunBranch,true))
+console.log(arizonaBank);
